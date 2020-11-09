4bacd3d54d3ede6f933b43ba54fa0028b6f5efb5
Courtney Vallentyne <covallen@microsoft.com>
2020-11-09 16:10:49 -0500

    ok

--- install.ps1
+++ install.ps1
@@ -1,5 +1,14 @@
+
+
+
+
 #Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     
 #Install Software
-choco install vscode -y
+# choco install vscode -y
+
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Vallentyne/DevVMEnvironment/main/Microsoft.DesktopAppInstaller.appxbundle'))
+
+
